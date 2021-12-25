89d4d5b64b0c2062daa81450b05f8cdc7d7bcfab
Thiago Domingues (tald) <thiagoald@gmail.com>
2021-12-25 16:27:54 -0300

    Add argparse Add --show-img (to show image after done) Add --show-scene (to show 3d scene after done) Add option to export to image (png, jpg)

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import numpy as np
 from PIL import Image
 from tqdm import tqdm
 from multiprocessing import Pool, cpu_count
+import argparse
 
 
 def intersect_rays(color, rays, triangle):
@@ -23,11 +24,25 @@ def intersect_rays(color, rays, triangle):
     return intersections
 
 
+def setup():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('scene', help='SDL scene')
+    parser.add_argument('--out', help='Output image')
+    parser.add_argument('--show-img', default=False, action='store_true')
+    parser.add_argument('--show-scene', default=False, action='store_true')
+    args = parser.parse_args()
+    return args
+
 def main():
-    scene = Scene(argv[1])
+    args = setup()
+    scene = Scene(args.scene)
     screen_pts = make_screen_pts(
         *scene.ortho, scene.width, scene.height)
     rays = make_rays(scene.eye, screen_pts)
+    print(f'Number of objects: {len(scene.objects)}')
+    print(
+        f'Number of triangles: {sum([len(o["geometry"].triangles) for o in scene.objects])}')
+    print(f'Number of rays: {len(rays)}')
     intersections = []
     n_cpus = cpu_count()
     results = []
@@ -46,13 +61,14 @@ def main():
                             (obj_color, rays[i_cpu::n_cpus], triangle)))
         for result in tqdm(results):
             intersections.extend(result.get())
-    print(f'Number of objects: {len(scene.objects)}')
-    print(
-        f'Number of triangles: {sum([len(o["geometry"].triangles) for o in scene.objects])}')
-    plot_scene(scene, rays, intersections)
+    if args.show_scene:
+        plot_scene(scene, rays, intersections)
     im = make_image(*scene.ortho, scene.width,
                     scene.height, intersections)
-    im.show()
+    if args.out is not None:
+        im.save(args.out)
+    if args.show_img:
+        im.show()
 
 
 if __name__ == '__main__':
